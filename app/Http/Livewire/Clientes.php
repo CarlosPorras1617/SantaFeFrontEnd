c7277067013052857326cd8b385e31e20d9390ec
CarlosPorras1617 <porrassainz17@gmail.com>
2023-06-21 20:48:20 -0700

    Avances

--- app/Http/Livewire/Clientes.php
+++ app/Http/Livewire/Clientes.php
@@ -1,19 +1,43 @@
 <?php
 
 namespace App\Http\Livewire;
-
 use Livewire\Component;
 use Illuminate\Support\Facades\Http;
 class Clientes extends Component
 {
-    public $idCliente;
+    //cualquier cambio de variable en esta parte, ejecuta la funcion de render y se actualzian los datos
+    public $currentPage = 1;
+    public $idClient;
     public $dataCreateClient = [];
     public $errors =  [];
 
     public function render()
     {
-        $response = Http::get('http://127.0.0.1:8000/api/clientes');
-        $clients = $response->json(["data"]);
-        return view('livewire.clientes', compact('clients'));
+        //obtiene los registros en base la pagina
+        $totalPaginated = Http::get('http://127.0.0.1:8000/api/clientes/activos?page=' . $this->currentPage);
+        //peticion a clientes sin paginacion para obtener el total de clientes para la paginacion
+        $response = Http::get('http://127.0.0.1:8000/api/clientes/');
+        //contamos los registros que hay de clientes
+        $totalClients = count($response->json());
+        //indicamos que los clientes que iran al front son los que se trajo la url paginados
+        $clients = $totalPaginated->json();
+        //current page para el front
+        $currentpage = $clients['current_page'];
+        return view('livewire.clientes', compact('clients', 'totalClients', 'currentpage'));
+    }
+    public function nextsPages($i)
+    {
+        //si mandamos la pagina nueva, cargara de nuevo la funcion render y renderizara la tabla en base a la nueva pagina
+        $this->currentPage = $i;
+    }
+    public function deleteClient($id){
+        $this->idClient = $id;
+        $response = Http::delete('http://127.0.0.1:8000/api/clientes/eliminar/'.$this->idClient);
+        if (!$response->successful()) {
+
+        }else{
+            $this->errors = $response->json();
+            redirect('/clientes');
+        }
     }
 }
--- resources/views/livewire/clientes.blade.php
+++ resources/views/livewire/clientes.blade.php
@@ -13,7 +13,8 @@
                     <div class="card-body bg-light" style="border-radius: 10px">
                         <table class="table white">
                             <thead>
-                                <tr style="font-family: 'Arial'; font-size: 28px; text-align:center">
+                                <tr
+                                    style="background-color: #AD7A78; font-family: 'Arial'; font-size: 28px; text-align:center">
                                     <th style="width: 15px">ID</th>
                                     <th>Nombre</th>
                                     <th>RFC</th>
@@ -24,7 +25,7 @@
                             </thead>
                             <tbody>
                                 <!-- for each para los usuarios -->
-                                @foreach ($clients as $client)
+                                @foreach ($clients['data'] as $client)
                                     <tr style="font-family: 'monospace'; font-size: 18px; text-align:center">
                                         <td>{{ $client['id'] }}</td>
                                         <td>{{ $client['nombre'] }}</td>
@@ -32,10 +33,12 @@
                                         <td>{{ $client['created_at'] }}</td>
                                         <td>{{ $client['updated_at'] }}</td>
                                         <td style="list-style: none; display: flex">
-                                            <a  class="btn btn-flat" style="margin: auto"><i class="large material-icons">create</i></a>
-                                            <a  class="btn btn-flat" style="margin: auto" href="clientes/{{$client['id']}}"><i class="large material-icons">delete</i> </button>
+                                            <a class="btn btn-flat" style="margin: auto"><i
+                                                    class="large material-icons">create</i></a>
+                                            <a class="btn btn-flat" style="margin: auto"
+                                                wire:click='deleteClient({{ $client['id'] }})'"><i
+                                                    class="large material-icons">delete</i> </button>
                                         </td>
-                                        <!-- <td>$cliente['facturas']['Nombre'][0]</td> -->
                                     </tr>
                                 @endforeach
                                 <br>
@@ -45,9 +48,36 @@
                     </div>
 
                     <!--FIN-->
+                    <ul class="pagination">
+                        @php
+                            //creamos cuantas divisiones tendra nuestro paginate diviendo el total de clientes / los mostrados en pantalla
+                            $totalPages = ceil($totalClients / 10);
+                        @endphp
+                        @for ($i = 1; $i <= $totalPages; $i++)
+                            <!--creamos los botones en a los numeros de registros cada uno con la funcion de next pages y mandando la pagina a la que navegaremos-->
+                            <li class="{{ $currentpage == $i ? 'active red' : 'waves-effect' }}">
+                                <a wire:click='nextsPages({{ $i }})' class="btn paginado"
+                                    style="{{ $currentpage == $i ? 'color: #752E2B;' : 'color: #752E2B;' }}">{{ $i }}</a>
+                            </li>
+                        @endfor
+                    </ul>
+
+                    </ul>
+
+
                 </div>
                 <div class="col s12 m12 l4">
-                    <h1>Formulario</h1>
+                    <div class="clienteForm center">
+                        <h3 class="subtitulos">CLIENTE</h3>
+                        <div class="input-field campos">
+                            <input id="nombre" type="text" class="validate">
+                            <label for="Nombre">Nombre</label>
+                        </div>
+                        <div class="input-field campos">
+                            <input id="rfc" type="text" class="validate">
+                            <label for="rfc">RFC</label>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

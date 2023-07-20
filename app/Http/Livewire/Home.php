039ded05762a5ced6d9e331ffbad43c3071ea22c
CarlosPorras1617 <porrassainz17@gmail.com>
2023-07-19 22:29:16 -0700

    Muestra Status de Tramite

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -3,11 +3,30 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class Home extends Component
 {
+    public $numEntrada;
+    public $tramite = [];
     public function render()
     {
+        $characterCount = strlen($this->numEntrada);
+        if ($characterCount == 11) {
+            //get tramite
+            $response = Http::get('http://127.0.0.1:8000/api/tramite/numEntrada/' . $this->numEntrada);
+
+            $this->tramite = $response->json();
+            $this->emit('cargando', $this->tramite);
+        }
         return view('livewire.home');
+    }
+
+    public function searchTramite()
+    {
+        //get tramite
+        $response = Http::get('http://127.0.0.1:8000/api/tramite/numEntrada/' . $this->numEntrada);
+
+        $this->tramite = $response->json();
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -83,39 +83,123 @@
                 <div class="col s10 m10 l10">
                     <form>
                         <div class="input-field col s8 cards ">
-                            <input type="number" id="numero" data-length="11">
+                            <input wire:model='numEntrada' type="number" id="numero" data-length="11">
                             <label for="numero">Numero de Entrada</label>
                         </div>
-                        <i class="material-icons prefix">search</i>
+                        <a href="#" class="icon-link modal-trigger" wire:click='searchTramite'>
+                            <i class="material-icons prefix">search</i>
+                        </a>
                     </form>
                 </div>
+                <div class="row">
+                    <div class="col s3 m3 l3">
+                        <p id="numEntrada"></p>
+                    </div>
+                    <div class="col s3 m3 l3">
+                        <p id="chofer"></p>
+                    </div>
+                    <div class="col s3 m3 l3">
+                        <p id="creado"></p>
+                    </div>
+                    <div class="col s3 m3 l3">
+                        <p id="actualizado"></p>
+                    </div>
+                </div>
+                <div id="botonBuscarOtro"></div>
             </div>
         </div>
         <footer class="page-footer">
             <div class="container">
-              <div class="row">
-                <div class="col l6 s12">
-                  <h5 class="white-text">ACCESOS DIRECTOS</h5>
-                  <p class="grey-text text-lighten-4">Navegación rapida a los apartados del sistema</p>
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">ACCESOS DIRECTOS</h5>
+                        <p class="grey-text text-lighten-4">Navegación rapida a los apartados del sistema</p>
+                    </div>
+                    <div class="col l4 offset-l2 s12">
+                        <h5 class="white-text">Links</h5>
+                        <ul>
+                            <li><a class="grey-text text-lighten-3" href="#!">CHOFERES</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">CONSULTAR TRAMITES</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">CLIENTES</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">CREAR TRÁMITE</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col l4 offset-l2 s12">
-                  <h5 class="white-text">Links</h5>
-                  <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!">CHOFERES</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">CONSULTAR TRAMITES</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">CLIENTES</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">CREAR TRÁMITE</a></li>
-                  </ul>
-                </div>
-              </div>
             </div>
             <div class="footer-copyright">
-              <div class="container">
-              © 2023 SANTA FE - TODOS LOS DERECHOS RESERVADOS
-              </div>
+                <div class="container">
+                    © 2023 SANTA FE - TODOS LOS DERECHOS RESERVADOS
+                </div>
             </div>
-          </footer>
+        </footer>
     </div>
 </div>
 
 
+<script>
+    document.addEventListener('livewire:load', function() {
+        Livewire.on('cargando', function(cargando) {
+
+            function formatTimestamp(timestamp) {
+                /*const dateObj = new Date(timestamp);
+
+                function getMonthName(monthNumber) {
+                    const monthNames = [
+                        'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+                        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+                    ];
+                    return monthNames[monthNumber - 1];
+                }
+
+                function formatNumber(number) {
+                    return number < 10 ? `0${number}` : number;
+                }
+
+                const year = dateObj.getFullYear();
+                const month = dateObj.getMonth() + 1;
+                const day = dateObj.getDate();
+                const hours = dateObj.getHours();
+                const minutes = dateObj.getMinutes();
+                const seconds = dateObj.getSeconds();
+
+                return `${day} de ${getMonthName(month)} de ${year}, ${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;*/
+                const dateObj = new Date(timestamp);
+
+                const options = {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                };
+
+                return dateObj.toLocaleString(undefined, options);
+            }
+
+
+            const fechaCreacion = formatTimestamp(cargando.created_at);
+            const fechaDeRecogido = formatTimestamp(cargando.updated_at);
+            //verificar si vino por el tramite
+            if (cargando.status == 1) {
+                actualizado.innerHTML = '<h5>Actualizado</h5><br>' + 'Pendiente';
+            }
+            if (cargando.status == 0) {
+                actualizado.innerHTML = '<h5>Estatus</h5><br>' + 'Recogido <br>' + fechaDeRecogido;
+            }
+
+
+            // Ejecutar la función que desees aquí, o mostrar un mensaje
+            botonBuscarOtro = document.getElementById('botonBuscarOtro');
+            numEntrada = document.getElementById('numEntrada');
+            chofer = document.getElementById('chofer');
+            creado = document.getElementById('creado');
+            actualizado = document.getElementById('actualizado');
+            numEntrada.innerHTML = '<h5>Num. Entrada:</h5><br>' + cargando.numEntrada;
+            chofer.innerHTML = '<h5>Chofer:</h5><br>' + cargando.chofer;
+            creado.innerHTML = '<h5>Trámite Creado</h5><br>' + fechaCreacion;
+            botonBuscarOtro.innerHTML = '<a class="btn pedimentoRTFormButton" href="/">BUSCAR OTRO</a>'
+            console.log(cargando);
+        });
+    });
+</script>
